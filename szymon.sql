a8ed70a6f3a019b7f08a9b08439c61779c65951f
Szymon Trofimiec <sztrofimiec@gmail.com>
2024-06-01 16:44:49 +0200

    naprawilem grupy_insert

--- szymon.sql
+++ szymon.sql
@@ -9,16 +9,15 @@ create or replace trigger grupy_upd_del
 before delete or update on grupy
 for each row execute procedure grupy_upd_del();
 ----------------------------------------------------
-create or replace function grupy_insert()
+create or replace function b_grupy_insert()
 returns trigger as
-$grupy_insert$
+$b_grupy_insert$
 declare
     licznik int;
     chetni cursor for select l.id_klienta from lista_oczekujacych l
 where l.data_rozpoczecia = new.data_rozpoczecia and l.id_odznaki = new.id_odznaki;
     rekord record;
     wskaźnik int := 0;
-
 begin
     select count(*) from lista_oczekujacych into licznik
 	where new.data_rozpoczecia = data_rozpoczecia and new.id_odznaki = id_odznaki;
@@ -26,16 +25,33 @@ begin
         raise exception 'za mało dzieci by utworzyć grupę';
         return null;
     end if;
-    open chetni;
-    insert into grupy(id_grupy, id_instuktora, id_odznaki, data_rozpoczecia, maks_dzieci, min_dzieci) values (new.id_grupy, new.id_instuktora, new.id_odznaki, new.data_rozpoczecia, new.maks_dzieci, new.min_dzieci);
+	return new;
+end;
+$b_grupy_insert$ language plpgsql;
+create or replace trigger grupy_insert before insert on grupy
+for each row execute procedure b_grupy_insert();
+---------------------------------------------------------------------------------------
+create or replace function aft_grupy_insert()
+returns trigger as
+$aft_grupy_insert$
+declare
+	licznik int;
+    chetni cursor for select l.id_klienta from lista_oczekujacych l
+where l.data_rozpoczecia = new.data_rozpoczecia and l.id_odznaki = new.id_odznaki;
+    rekord record;
+    wskaźnik int := 0;
+begin
+	select count(*) from lista_oczekujacych into licznik
+	where new.data_rozpoczecia = data_rozpoczecia and new.id_odznaki = id_odznaki;
+	open chetni;
     while wskaźnik < new.maks_dzieci and wskaźnik < licznik loop
         fetch from chetni into rekord;
         insert into dzieci_grupy (id_klienta, id_grupy) values (rekord.id_klienta, new.id_grupy);
         delete from lista_oczekujacych where current of chetni;
-        wskaznik := wskaznik + 1;
+        wskaźnik := wskaźnik + 1;
     end loop;
-    return null;
+    return new;
 end;
-$grupy_insert$ language plpgsql;
-create or replace trigger grupy_insert before insert on grupy
-for each row execute procedure grupy_insert();
+$aft_grupy_insert$ language plpgsql;
+create or replace trigger aft_grupy_insert after insert on grupy
+for each row execute procedure aft_grupy_insert();
