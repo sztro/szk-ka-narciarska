fe9a0d46b60cf0221f075b930ffb4abcd9d8e2ae
Szymon Trofimiec <sztrofimiec@gmail.com>
2024-06-08 21:15:22 +0200

    s

--- ASU/src/main/java/main/User.java
+++ ASU/src/main/java/main/User.java
@@ -2,7 +2,7 @@ package main;
 
 public final class User {
     private User(){}
-    public static final String URL = "jdbc:postgresql://localhost:5432/sipuk";
-    public static final String USERNAME = "sipuk";
-    public static final String PASSWORD = "123";
+    public static final String URL = "jdbc:postgresql://localhost:5432/NAZWA-BAZY";
+    public static final String USERNAME = "LOGIN";
+    public static final String PASSWORD = "HASLO";
 }
